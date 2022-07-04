896dbbacb38dda533007dae973a833e14eac3f1f
ThamizharasanT <96743694+ThamizharasanT@users.noreply.github.com>
2022-07-04 16:29:31 +0530

    Create Custom Stack horizontal and vertical position.swift

---
+++ Custom Stack horizontal and vertical position.swift
@@ -0,0 +1,8 @@
+    func Stack(){
+        mainStack.axis = .horizontal
+        mainStack.distribution = .fillEqually
+        mainStack.alignment = .fill
+        mainStack.spacing = 5
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStack)
+    }
